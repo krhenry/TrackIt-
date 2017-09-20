e60f6d3456b2edc110377b6ed7c1b7e4b4bcbc2c
Kyle Henry <kyle.henry@emergencetek.com>
2017-09-20 16:11:12 -0400

    Removed duplicate code.

--- MonthlyFlashReport/Program.cs
+++ MonthlyFlashReport/Program.cs
@@ -32,13 +32,6 @@ namespace MonthlyFlashReport
 
             try
             {
-                //Declare Variables and provide values
-                //string FileNamePart = "TrackIt Flash Report";//Datetime will be added to it
-                //string DestinationFolder = @"C:\Clients\_NONGIT\OLV\";
-                //string StoredProcedureName = "uspSelectAllFromWoundType";//Provide SP name,you Can provide with Parameter if you like
-                //string FileDelimiter = ","; //You can provide comma or pipe or whatever you like
-                //string FileExtension = ".csv"; //Provide the extension you like such as .txt or .csv
-                                               //\nDefault Month is set to: " + DateTime.Now.AddMonths(-1).ToString("MMMM"));
                 bool validDate;
 
                 Console.WriteLine("Console Application for TrackIt Flash Report.\nDefault Start and End dates will be set to last month: " + DateTime.Now.AddMonths(-1).ToString("MMMM") + "\n");
@@ -69,58 +62,6 @@ namespace MonthlyFlashReport
                 ExportToCSV(StartDate, EndDate.ToString("d"));
                 Console.ReadLine();
 
-                //Create Connection to SQL Server in which you like to load files
-                //SqlConnection SQLConnection = new SqlConnection();
-                //SQLConnection.ConnectionString = LineConnection;
-
-                ////Execute Stored Procedure and save results in data table
-                //string query = "EXEC " + StoredProcedureName + " " + "'Surgical Wound'" + "," + "'Import'";
-                //SqlCommand cmd = new SqlCommand(query, SQLConnection);
-                //SQLConnection.Open();
-                //DataTable d_table = new DataTable();
-                //d_table.Load(cmd.ExecuteReader());
-                //SQLConnection.Close();
-
-                ////Prepare the file path 
-                //StartDate = StartDate.Replace("/", "_");
-                ////defaultEndDate = defaultEndDate.ToString("d").Replace("/", "_");
-                //string FileFullPath = DestinationFolder + "\\" + FileNamePart + "_" + StartDate + " to " + EndDate.ToString("d").Replace("/","_") + FileExtension;
-
-                //StreamWriter sw = null;
-                //sw = new StreamWriter(FileFullPath, false);
-
-                //// Write the Header Row to File
-                //int ColumnCount = d_table.Columns.Count;
-                //for (int ic = 0; ic < ColumnCount; ic++)
-                //{
-                //    sw.Write(d_table.Columns[ic]);
-                //    if (ic < ColumnCount - 1)
-                //    {
-                //        sw.Write(FileDelimiter);
-                //    }
-                //}
-                //sw.Write(sw.NewLine);
-
-                //// Write All Rows to the File
-                //foreach (DataRow dr in d_table.Rows)
-                //{
-                //    for (int ir = 0; ir < ColumnCount; ir++)
-                //    {
-                //        if (!Convert.IsDBNull(dr[ir]))
-                //        {
-                //            sw.Write(dr[ir].ToString());
-                //        }
-                //        if (ir < ColumnCount - 1)
-                //        {
-                //            sw.Write(FileDelimiter);
-                //        }
-                //    }
-                //    sw.Write(sw.NewLine);
-
-                //}
-
-                //sw.Close();
-
             }
 
             catch (Exception ex)
@@ -150,8 +91,8 @@ namespace MonthlyFlashReport
                 Console.WriteLine("Invalid date: \"{0}\"", date);
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("Enter valid date: MM/dd/yyyy");
-                string b = Console.ReadLine();
-                DateValidation(b);
+                string dt = Console.ReadLine();
+                DateValidation(dt);
                 return false;
             }
         }
